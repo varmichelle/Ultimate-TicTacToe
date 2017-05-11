4a99c1b0ed47e38d2fa5ae5d4ef7edf0e587e7cd
Michelle Li <michellecarolynclark@gmail.com>
2017-05-11 07:31:09 -0700

    base files added

---
+++ Ultimate-TicTacToe/src/HumanPlayer.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Maya Mistry
+ * @period 1
+ * 
+ */
+public class HumanPlayer extends Player {
+	protected Scanner input;
+
+	/**
+	 * Constructor that passes through the UltimateTicTacToeBoard object and the
+	 * GamePiece object to allow a human player to make a move on it.
+	 * 
+	 * @param b
+	 *            - board used to play the game
+	 * @param p
+	 *            - game piece used (x or o)
+	 */
+	public HumanPlayer(UltimateTicTacToeBoard b, GamePiece p) {
+		super(b, p);
+	}
+
+	/**
+	 * This abstract void method allows a human player or an AI to make a move
+	 * based off of whether or not it is valid to make a move. (Implemented
+	 * version for human player)
+	 * 
+	 */
+	public void makeAMove() {
+		input = new Scanner(System.in);
+		int outsideR = input.nextInt();
+		int outsideC = input.nextInt();
+		int insideR = input.nextInt();
+		int insideC = input.nextInt();
+		board.setPieceAt(outsideR, outsideC, insideR, insideC,
+						thisPlayersPiece);
+	}
+}
